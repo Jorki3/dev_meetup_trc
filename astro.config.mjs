6b63677e24d425a4a1dc6e234076ca7d8717e25f
Jorki3 <eduardomttzt01@outlook.com>
2024-10-05 00:12:49 -0600

    Astro config for cors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
-  output: 'server',
-  adapter: netlify()
+  output: "server",
+  adapter: netlify(),
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name]-[hash].js",
+        },
+      },
+    },
+  },
 });
