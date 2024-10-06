03f755a715b03123131128524dfe8715774d29d8
Jorki3 <eduardomttzt01@outlook.com>
2024-10-06 16:22:17 -0600

    Logout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,18 @@
 import type { APIRoute } from "astro";
+import { supabase } from "../../../lib/supabase"; // Importa supabase
 
-export const GET: APIRoute = async ({ cookies, redirect }) => {
+export const POST: APIRoute = async ({ cookies, redirect }) => {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error("Error al cerrar sesión:", error.message); // Agrega esto para depurar
+    return new Response(error.message, { status: 500 });
+  }
+
+  // Eliminar las cookies
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
+
+  // Redirigir a la página principal después de cerrar sesión
   return redirect("/");
 };
